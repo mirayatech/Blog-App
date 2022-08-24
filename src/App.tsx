362972b1c99bc29b04635ed274683314e30f0887
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-24 18:32:08 +0200

    setting up react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { CreatePost } from "./pages/CreatePost";
+
 export function App() {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/createpost" element={<CreatePost />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
