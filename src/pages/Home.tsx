3a047eada7a31fa6850f359dbadb0e0a195e599e
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-25 10:24:59 +0200

    feat: displaying post everytime we make them

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,39 @@
+import { useState, useEffect } from "react";
+import { getDocs, collection, doc } from "firebase/firestore";
+import { db } from "../firebase-config";
+
 export function Home() {
-  return <div>Home</div>;
+  const [postLists, setPostLists] = useState<
+    {
+      author: { id: string; name: string };
+      id: string;
+      postText: string;
+      title: string;
+    }[]
+  >([]);
+  const postCollectionRefrence = collection(db, "Post");
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postCollectionRefrence);
+      console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+
+      setPostLists(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getPosts();
+  }, []);
+
+  return (
+    <div className="home__page">
+      {postLists.map((post) => {
+        return (
+          <div key={post.id} className="post">
+            <h1>{post.title}</h1>
+            <p>{post.postText}</p>
+            <span>{post.author.name}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
