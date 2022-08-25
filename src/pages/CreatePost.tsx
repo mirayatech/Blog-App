3514189929b7cd22f01125e3f30406981507982b
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-25 08:21:43 +0200

    style: add styling to creatpost layout

--- src/pages/CreatePost.tsx
+++ src/pages/CreatePost.tsx
@@ -1,3 +1,19 @@
+import "../styles/createpost.css";
+
 export function CreatePost() {
-  return <div>createPost</div>;
+  return (
+    <div className="createpost">
+      <div className="createpost__container">
+        <h1>Create a post</h1>
+        <div className="line"></div>
+        <div className="input__wrapper">
+          <input type="text" name="title" placeholder="Title..." />
+        </div>
+        <div className="input__wrapper">
+          <textarea placeholder="Post..." />
+        </div>
+        <button className="createpost__button">Share Post</button>
+      </div>
+    </div>
+  );
 }
