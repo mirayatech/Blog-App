eba0a6edf1773d495996585a3c5125cfea643a48
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-25 08:48:25 +0200

    feat: creating blog  blog post (save it to firestore)

--- src/pages/CreatePost.tsx
+++ src/pages/CreatePost.tsx
@@ -1,18 +1,51 @@
+import { useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { db, auth } from "../firebase-config";
 import "../styles/createpost.css";
+import { useNavigate } from "react-router-dom";
 
 export function CreatePost() {
+  const [title, setTitle] = useState("");
+  const [postText, setPostText] = useState("");
+
+  const postCollectionRefrence = collection(db, "Post");
+  let navigate = useNavigate();
+
+  const createPost = async () => {
+    await addDoc(postCollectionRefrence, {
+      title: title,
+      postText: postText,
+      author: { name: auth.currentUser?.displayName, id: auth.currentUser.uid },
+    });
+    navigate("/");
+  };
+
   return (
     <div className="createpost">
       <div className="createpost__container">
         <h1>Create a post</h1>
         <div className="line"></div>
         <div className="input__wrapper">
-          <input type="text" name="title" placeholder="Title..." />
+          <input
+            onChange={(event) => {
+              setTitle(event.target.value);
+            }}
+            type="text"
+            name="title"
+            placeholder="Title..."
+          />
         </div>
         <div className="input__wrapper">
-          <textarea placeholder="Post..." />
+          <textarea
+            onChange={(event) => {
+              setPostText(event.target.value);
+            }}
+            placeholder="Post..."
+          />
         </div>
-        <button className="createpost__button">Share Post</button>
+        <button onClick={createPost} className="createpost__button">
+          Share Post
+        </button>
       </div>
     </div>
   );
