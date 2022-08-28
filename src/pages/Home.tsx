82d3e7b0251817970addef2d9451c5982c4da32a
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-28 13:30:24 +0200

    refactor: create on snapshot and remove unnecessary code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,9 @@
 import { useState, useEffect } from "react";
 import { IoMdTrash } from "react-icons/io";
 import {
-  getDocs,
   collection,
-  doc,
-  deleteDoc,
   CollectionReference,
+  onSnapshot,
 } from "firebase/firestore";
 import "../styles/home.css";
 import { firebaseAuth, firebaseDb } from "../lib/firebase";
@@ -18,48 +16,32 @@ type Post = {
 };
 
 export function Home() {
-  const [postLists, setPostLists] = useState<Post[]>([]);
+  const [posts, setPosts] = useState<Post[]>([]);
   const postsCollectionRef = collection(
     firebaseDb,
     "posts"
   ) as CollectionReference<Post>;
 
   useEffect(() => {
-    const getPosts = async () => {
-      const data = await getDocs<Post>(postsCollectionRef);
-      setPostLists(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
-    getPosts();
+    onSnapshot(postsCollectionRef, (snapshot) =>
+      setPosts(snapshot.docs.map((doc) => doc.data()))
+    );
   }, []);
-
-  const deletePost = async (id: any) => {
-    const postDoc = doc(firebaseDb, `posts/${id}`);
-    await deleteDoc(postDoc);
-  };
 
   return (
     <div className="home__page">
-      {postLists.map(({ id, title, postText, author }) => {
+      {posts.map(({ title, id, postText, author }) => {
         return (
           <div key={id} className="post">
-            <div className="header">
-              <h1>{title}</h1>{" "}
-              {author.id === firebaseAuth.currentUser?.uid && (
-                <button
-                  className="delete__post"
-                  onClick={() => {
-                    deletePost(id);
-                  }}
-                >
-                  <IoMdTrash />
-                </button>
-              )}
-            </div>
+            <header className="header">
+              <h1>{title}</h1>
+              <IoMdTrash />
+            </header>
             <div className="line"></div>
             <div className="post__text">
               <p>{postText}</p>
             </div>
-            <span className="post__author">Author: {author.name}</span>
+            <span>{author.name}</span>
           </div>
         );
       })}
