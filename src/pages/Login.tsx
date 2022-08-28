108234e8cb8baf3c59eaa63bb2eb1431a8926884
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-28 17:47:32 +0200

    refactor: remove state and unnecessary code

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,32 +1,36 @@
 import { FcGoogle } from "react-icons/fc";
-import { auth, provider } from "../firebase-config";
-import { signInWithPopup } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
+import { onAuthStateChanged, signInWithPopup } from "firebase/auth";
 import "../styles/navbar.css";
+import { firebaseAuth, googleAuthProvider } from "../lib/firebase";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
-type LoginProps = {
-  setIsAuth: React.Dispatch<React.SetStateAction<boolean>>;
-};
-
-export function Login({ setIsAuth }: LoginProps) {
-  let navigate = useNavigate();
+export function Login() {
+  const navigate = useNavigate();
 
   const signInWithGoogle = () => {
-    signInWithPopup(auth, provider).then(() => {
-      localStorage.setItem("isAuth", true);
-      setIsAuth(true);
-
+    signInWithPopup(firebaseAuth, googleAuthProvider).then(() => {
       navigate("/");
     });
   };
 
+  // useEffect(() => {
+
+  // }, []);
+
   return (
-    <div className="login-page">
-      <h1>In order to create a post, you must sign in with Google.</h1>
-      <button className="login__button" onClick={signInWithGoogle}>
-        <FcGoogle className="login__button--icon" />
-        Sign in with Google
-      </button>
-    </div>
+    <>
+      {firebaseAuth.currentUser ? (
+        ""
+      ) : (
+        <div className="login-page">
+          <h1>In order to create a post, you must sign in with Google.</h1>
+          <button className="login__button" onClick={signInWithGoogle}>
+            <FcGoogle className="login__button--icon" />
+            Sign in with Google
+          </button>
+        </div>
+      )}
+    </>
   );
 }
