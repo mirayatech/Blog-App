7b08be5a84faa3c69f7ec5eaec786a134017e6eb
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-28 17:46:42 +0200

    fix: when window refresh, user stays att the same page, if it's logged in

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,37 @@ import { Login } from "./pages/Login";
 import { CreatePost } from "./pages/CreatePost";
 import { Navbar } from "./component/Navbar";
 import { firebaseAuth } from "./lib/firebase";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
 
 export function App() {
-  console.log(firebaseAuth);
+  const [isUserSignedIn, setIsUserSignedIn] = useState(true);
+
+  useEffect(() => {
+    onAuthStateChanged(firebaseAuth, (currentUser) => {
+      if (currentUser) {
+        return setIsUserSignedIn(true);
+      } else {
+        setIsUserSignedIn(false);
+      }
+    });
+  }, []);
 
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/createpost" element={<CreatePost />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <Navbar isUserSignedIn={isUserSignedIn} />
+
+      {isUserSignedIn === true ? (
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/createpost" element={<CreatePost />} />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      )}
     </Router>
   );
 }
--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,14 +1,18 @@
 import { NavLink, useNavigate } from "react-router-dom";
+import { firebaseAuth } from "../lib/firebase";
 import { signOut } from "firebase/auth";
 import "../styles/navbar.css";
-import { firebaseAuth } from "../lib/firebase";
 
-export function Navbar() {
+type NavbarProps = {
+  isUserSignedIn: boolean;
+};
+
+export function Navbar({ isUserSignedIn }: NavbarProps) {
   const navigate = useNavigate();
 
   const signUserOut = () => {
     signOut(firebaseAuth).then(() => {
-      navigate("./");
+      navigate("/login");
     });
   };
 
@@ -16,7 +20,7 @@ export function Navbar() {
     <nav>
       <NavLink to="/">Home</NavLink>
 
-      {firebaseAuth.currentUser ? (
+      {isUserSignedIn === true ? (
         <>
           <NavLink to="/createpost">Create Post</NavLink>
           <button className="logout__button" onClick={signUserOut}>
